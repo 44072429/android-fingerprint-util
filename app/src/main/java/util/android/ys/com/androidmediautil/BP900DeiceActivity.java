20c03f7874614ef46dd096eac252855b3438e793
44072429 <44072429@qq.com>
2018-09-28 18:05:30 +0800

    BP900 指纹

--- app/src/main/java/util/android/ys/com/androidmediautil/BP900DeiceActivity.java
+++ app/src/main/java/util/android/ys/com/androidmediautil/BP900DeiceActivity.java
@@ -2,11 +2,20 @@ package util.android.ys.com.androidmediautil;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,19 +23,26 @@ import butterknife.OnClick;
 import util.android.ys.com.fingerprnt.FingerUtilV2;
 import util.android.ys.com.fingerprnt.Fingerprint;
 
-public class BP900DeiceActivity extends AppCompatActivity {
+public class BP900DeiceActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     @BindView(R.id.image)
     ImageView image;
     @BindView(R.id.tv_texet)
     TextView tvTexet;
 
+    // 语音
+    private WeakReference<TextToSpeech> ttsRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bp900_deice);
         ButterKnife.bind(this);
-        FingerUtilV2.globalInit(this , "BP900");
+        FingerUtilV2.globalInit(this, "BP900");
+
+        // 语音相关
+        ttsRef = new WeakReference<TextToSpeech>(new TextToSpeech(BP900DeiceActivity.this, BP900DeiceActivity.this));
+
     }
 
     @Override
@@ -36,14 +52,38 @@ public class BP900DeiceActivity extends AppCompatActivity {
         FingerUtilV2.globalRelease();
     }
 
-    @OnClick({R.id.Btn1, R.id.Btn2, R.id.Btn3})
+    @OnClick({R.id.Btn1, R.id.Btn2, R.id.Btn3, R.id.Btn4})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.Btn1:
+                tvTexet.setText("请验证指纹");
+                FingerUtilV2.fingerVerifyStart(new Fingerprint.FingerprinEventlistener() {
+                    @Override
+                    public void onSuccess(int captureMode, final byte[] imageBuffer, int[] imageAttributes, final byte[] templateBuffer) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Bitmap bmp = BitmapFactory.decodeByteArray(imageBuffer, 0, imageBuffer.length);
+                                image.setImageBitmap(bmp);
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onFailure(final String err) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvTexet.setText(err);
+                            }
+                        });
+                    }
+                });
 
                 break;
             case R.id.Btn2:
-
+                FingerUtilV2.fingerVerifyStop();
                 break;
             case R.id.Btn3:
 
@@ -54,7 +94,7 @@ public class BP900DeiceActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                tvTexet.setText("请再按" + timesLeft  + "次");
+                                tvTexet.setText("请再按" + timesLeft + "次");
                                 Bitmap bmp = BitmapFactory.decodeByteArray(imageBuffer, 0, imageBuffer.length);
                                 image.setImageBitmap(bmp);
                             }
@@ -88,6 +128,21 @@ public class BP900DeiceActivity extends AppCompatActivity {
 
                 }, 3);
                 break;
+            case R.id.Btn4:
+                ttsRef.get().speak("说话", TextToSpeech.QUEUE_FLUSH, null);
+                break;
         }
     }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = ttsRef.get().setLanguage(Locale.CHINESE);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+//                showMessage("不支持中文语音");
+            }
+        }
+    }
+
 }
--- fingerprnt-util/src/main/java/util/android/ys/com/fingerprnt/FingerprintBP900.java
+++ fingerprnt-util/src/main/java/util/android/ys/com/fingerprnt/FingerprintBP900.java
@@ -28,13 +28,13 @@ public class FingerprintBP900 implements Fingerprint {
      * BP900指纹录入
      *
      * @param fingerprinEnrollEventlistener
-     * @param times                         需要录入多少次指纹，建议3
+     * @param times 需要录入多少次指纹，建议3
      * @return
      */
     @Override
     public boolean fingerEnrollStart(FingerprinEnrollEventlistener fingerprinEnrollEventlistener, int times) {
         this.fingerprinEnrollEventlistener = fingerprinEnrollEventlistener;
-        if(regFingerThread == null || regFingerThread.isAlive() == false){
+        if (regFingerThread == null || regFingerThread.isAlive() == false) {
             regFingerThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -52,13 +52,14 @@ public class FingerprintBP900 implements Fingerprint {
 
     /**
      * BP900指纹验证
+     *
      * @param fingerprinEventlistener
      * @return
      */
     @Override
     public boolean fingerVerifyStart(FingerprinEventlistener fingerprinEventlistener) {
         this.fingerprinEventlistener = fingerprinEventlistener;
-        if(regFingerThread == null){
+        if (regFingerThread == null || regFingerThread.isAlive() == false) {
             regFingerThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -72,7 +73,10 @@ public class FingerprintBP900 implements Fingerprint {
 
     @Override
     public void fingerVerifyStop() {
-
+        if (regFingerThread != null || regFingerThread.isAlive() == true) {
+            Device.cancel();
+            regFingerThread.interrupt();
+        }
     }
 
     private String newGBKString(byte[] bytes) {
@@ -84,6 +88,12 @@ public class FingerprintBP900 implements Fingerprint {
     }
 
     private void regFinger() {
+
+        if(regFingerThread.isInterrupted()){
+            System.out.println("线程被停止了！");
+            return;
+        }
+
         byte[] image = new byte[2000 + 152 * 200];
         byte[] message = new byte[200];
         byte[][] tz = new byte[3][513];
@@ -103,8 +113,8 @@ public class FingerprintBP900 implements Fingerprint {
                     String str = newGBKString(message);
                     Log.d("aaa", "Str == >" + str);
                     Log.d("aaa", "r   == >" + str);
-                    fingerprinEnrollEventlistener.onFailure("超时 重新录入");
-
+//                    fingerprinEnrollEventlistener.onFailure("超时 重新录入");
+                    fingerprinEnrollEventlistener.onFailure(str);
                     if (fingerprinEnrollEventlistener.onCaptureTime()) {
                         // 超时回调 如果true 继续采集 重新启动指纹
                         regFinger();
@@ -139,7 +149,77 @@ public class FingerprintBP900 implements Fingerprint {
         regFinger();
     }
 
+// 指纹和录入一起
+//    private void regFinger() {
+//        byte[] image = new byte[2000 + 152 * 200];
+//        byte[] message = new byte[200];
+//        byte[][] tz = new byte[3][513];
+//        byte[] mb = new byte[513];
+//        Device.openFinger(message);
+//        Device.openRfid(message);
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//        }
+//        try {
+//            for (int i = 0; i < 3; i++) {
+//                image = new byte[2000 + 152 * 200];
+//                Log.d("aaa", "    采集指纹,请按手指(第" + (i + 1) + "次)...");
+//                int r = Device.getImage(10000, image, message);
+//                if (r != 0) {
+//                    String str = newGBKString(message);
+//                    Log.d("aaa", "Str == >" + str);
+//                    Log.d("aaa", "r   == >" + str);
+//                    fingerprinEnrollEventlistener.onFailure("超时 重新录入");
+//
+//                    if (fingerprinEnrollEventlistener.onCaptureTime()) {
+//                        // 超时回调 如果true 继续采集 重新启动指纹
+//                        regFinger();
+//                    }
+//                    return;
+//                }
+//                r = Device.ImageToFeature(image, tz[i], message);
+//                if (r != 0) {
+//                    String str = newGBKString(message);
+//                    Log.d("aaaa", "str == >" + str);
+//                    Log.d("aaaa", "r   == >" + str);
+//                    return;
+//                }
+//                if(isLr == false){
+//                    fingerprinEventlistener.onSuccess(0, image, null, tz[i]);
+//                }else {
+//                    fingerprinEnrollEventlistener.onSuccess(0, image, null, null, i + 1);
+//                }
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        if(isLr == false){
+//
+//        } else {
+//            // 整合新指纹
+//            int r = Device.FeatureToTemp(tz[0], tz[1], tz[2], mb, message);
+//            if (r != 0) {
+//                String str = newGBKString(message);
+//                Log.d("aaaa", "str == >" + str);
+//                Log.d("aaaa", "r   == >" + str);
+//                fingerprinEnrollEventlistener.onFailure("请按同一个指纹三次");
+//                regFinger();
+//                return;
+//            }
+//            Log.d("aaaa", "录入成功");
+//            fingerprinEnrollEventlistener.onEnrollSuccess(mb);
+//        }
+//        regFinger();
+//    }
+
     private void verify() {
+
+        if(regFingerThread.isInterrupted()){
+            System.out.println("线程被停止了！");
+            return;
+        }
 
         byte[] image = new byte[2000 + 152 * 200];
         byte[] message = new byte[200];
@@ -152,31 +232,33 @@ public class FingerprintBP900 implements Fingerprint {
         } catch (InterruptedException e) {
         }
         try {
-            image = new byte[2000 + 152 * 200];
-            int r = Device.getImage(10000, image, message);
-            if (r != 0) {
-                String str = newGBKString(message);
-                Log.d("aaa", "Str == >" + str);
-                Log.d("aaa", "r   == >" + str);
-                fingerprinEventlistener.onFailure("超时 重新录入");
-
-                if (fingerprinEnrollEventlistener.onCaptureTime()) {
-                    // 超时回调 如果true 继续采集 重新启动指纹
+            while (true) {
+                image = new byte[2000 + 152 * 200];
+                int r = Device.getImage(10000, image, message);
+                if (r != 0) {
+                    String str = newGBKString(message);
+                    Log.d("aaa", "Str == >" + str);
+                    Log.d("aaa", "r   == >" + str);
+//                    fingerprinEventlistener.onFailure("超时 重新录入");
+                    fingerprinEventlistener.onFailure(str);
                     regFinger();
+//                    if (fingerprinEnrollEventlistener.onCaptureTime()) {
+//                        // 超时回调 如果true 继续采集 重新启动指纹
+//                        regFinger();
+//                    }
+                    return;
                 }
-                return;
+                r = Device.ImageToFeature(image, tz[0], message);
+                if (r != 0) {
+                    String str = newGBKString(message);
+                    Log.d("aaaa", "str == >" + str);
+                    Log.d("aaaa", "r   == >" + str);
+                    return;
+                }
+                fingerprinEventlistener.onSuccess(0, image, null, tz[0]);
             }
-            r = Device.ImageToFeature(image, tz[0], message);
-            if (r != 0) {
-                String str = newGBKString(message);
-                Log.d("aaaa", "str == >" + str);
-                Log.d("aaaa", "r   == >" + str);
-                return;
-            }
-            fingerprinEventlistener.onSuccess(0, image, null, tz[0]);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-
 }
